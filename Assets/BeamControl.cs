23906510fcabeb96d6d5605d1acf9cf4f759d980
library_midori <hung.phamduy.hust@gmail.com>
2015-07-30 08:41:51 +0700

    Laser effection is okay. Now refactor the code and try to implement new laser

---
+++ Assets/BeamControl.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamControl : MonoBehaviour
+{
+    public Transform parrentTransform;
+    public Transform endPoint;
+    public Vector3 direction;
+    public LineRenderer laserLine;
+    public LayerMask mask;
+
+    private RaycastHit hitInfo;
+
+    void Start()
+    {
+        laserLine.SetWidth(0.2f, 0.2f);
+        direction = Vector3.right;
+    }
+
+    void Update()
+    {
+        traceLine();
+        //detectReflexion();
+        //detectObstacle();
+    }
+
+    private void traceLine()
+    {
+        Debug.DrawRay(parrentTransform.position, direction, Color.red);
+
+        //if (Physics.Raycast(parrentTransform.position, comingVector, out hit, mask))
+        if (Physics.Raycast(parrentTransform.position, direction, out hitInfo))
+        {
+            laserLine.SetVertexCount(2);
+            laserLine.SetPosition(0, parrentTransform.position);
+            laserLine.SetPosition(1, hitInfo.point);
+
+            if (true)
+            {
+                Vector3 incomingVector = hitInfo.point - parrentTransform.position;
+                Vector3 reflectVector = Vector3.Reflect(incomingVector, hitInfo.normal);
+                Debug.DrawRay(hitInfo.point, reflectVector, Color.green);
+                laserLine.SetVertexCount(3);
+                laserLine.SetPosition(1, hitInfo.point);
+                laserLine.SetPosition(2, hitInfo.point + 2 * reflectVector);
+            }
+            return;
+        }
+
+        laserLine.SetVertexCount(2);
+        laserLine.SetPosition(0, parrentTransform.position);
+        laserLine.SetPosition(1, parrentTransform.position + 20 * direction);
+    }
+
+    private void detectReflexion()
+    {
+        Vector3 comingVector = endPoint.position - parrentTransform.position;
+
+        //Debug.DrawLine(parrentTransform.position, endPoint.position, Color.red);
+        Debug.DrawRay(parrentTransform.position, direction, Color.red);
+        laserLine.SetPosition(0, parrentTransform.position);
+        laserLine.SetPosition(1, endPoint.position);
+
+        //if (Physics.Raycast(parrentTransform.position, comingVector, out hit, mask))
+        if (Physics.Raycast(parrentTransform.position, comingVector, out hitInfo))
+        {
+            Vector3 incomingVector = hitInfo.point - parrentTransform.position;
+            Vector3 reflectVector = Vector3.Reflect(incomingVector, hitInfo.normal);
+            Debug.DrawRay(hitInfo.point, reflectVector, Color.green);
+            laserLine.SetVertexCount(3);
+            laserLine.SetPosition(1, hitInfo.point);
+            laserLine.SetPosition(2, hitInfo.point + 2 * reflectVector);
+        }
+    }
+
+    private void detectObstacle()
+    {
+        Vector3 comingVector = endPoint.position - parrentTransform.position;
+
+        Debug.DrawLine(parrentTransform.position, endPoint.position, Color.red);
+
+
+        //if (Physics.Raycast(parrentTransform.position, comingVector, out hit, mask))
+        if (Physics.Raycast(parrentTransform.position, comingVector, out hitInfo))
+        {
+            laserLine.SetPosition(0, parrentTransform.position);
+            laserLine.SetPosition(1, hitInfo.point);
+            //laserLine.SetPosition(2, hitInfo.point);
+            laserLine.SetVertexCount(2);
+            //Vector3 incomingVector = hitInfo.point - parrentTransform.position;
+            //Vector3 reflectVector = Vector3.Reflect(incomingVector, hitInfo.normal);
+            //Debug.DrawRay(hitInfo.point, reflectVector, Color.green);
+
+            //laserLine.SetPosition(1, hitInfo.point);
+            //laserLine.SetPosition(2, hitInfo.point + 2 * reflectVector);
+        }
+    }
+}
---
+++ Assets/MovingObject.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour
+{
+    /// <summary>
+    /// Flag to control the object can be drag or not
+    /// </summary>
+    public bool isDragable = true;
+
+    /// <summary>
+    /// Temporary variable to improving efficience purpose.
+    /// </summary>
+    private Vector3 curScreenPos;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        if (isDragable)
+        {
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            setCurrentScreenPoint(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(curScreenPos);
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragable)
+        {
+            setCurrentScreenPoint(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPos) + offset;
+            transform.position = curPosition;
+        }
+    }
+
+    /// <summary>
+    /// Improving the efficience by reducing the new operating in Vector3 objects.
+    /// </summary>
+    /// <param name="x">x coordinate value</param>
+    /// <param name="y">y coordinate value</param>
+    /// <param name="z">z coordinate value</param>
+    private void setCurrentScreenPoint(float x, float y, float z)
+    {
+        curScreenPos.x = x;
+        curScreenPos.y = y;
+        curScreenPos.z = z;
+    }
+}
